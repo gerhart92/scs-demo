c38878eb5fdcf6383ee81006cfd26729f7d19f05
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-09-08 22:01:21 +0300

    Initial Sitecore Content Serialiyation setup

---
+++ src/Feature/Metadata/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Sitecore.Feature.Metadata")]
+[assembly: AssemblyProduct("Sitecore.Feature.Metadata")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("780ea6b4-4085-46f0-aded-c751bdc41ce1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Project/SCSDemo/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Sitecore.Test.Website")]
+[assembly: AssemblyProduct("Sitecore.Test.Website")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: ComVisible(false)]
+[assembly: Guid("339842a1-d1f5-4c29-9a2a-71974661c3a1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
